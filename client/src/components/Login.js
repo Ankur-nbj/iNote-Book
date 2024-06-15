a08d90c2b3c4263e0e84bbb801ae566c72775d40
Ankur Gangwar <108694248+Ankur-nbj@users.noreply.github.com>
2024-06-15 18:41:01 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -34,7 +34,7 @@ const Login = () => {
 
   const handleFormSubmit = async (values, onSubmitProps) => {
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://inote-book-ohjb.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: values.email, password: values.password }),
